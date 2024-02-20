0d630970b449c0b41fc397a8003232477013cfed
Janakan Sureshraj <janakansureshraj2@gmail.com>
2024-02-20 07:37:28 -0500

    added halal supplier logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <section className="h-wrapper">
             <div className="flexCenter paddings innerWidth h-container">
                 <a href="">
-                    <img src="./logo.jpg" alt="logo" width={55}/>
+                    <img src="./logo.jpg" alt="logo" width={50}/>
                 </a>
                 
                 <div className="flexCenter h-menu"> 
@@ -15,7 +15,10 @@ const Header = () => {
                         <a href="https://gosnappy.io/owa/snappy/detail/G9623/9623/">Menu</a>
                         <span> 
                         <a>
-                            <img src="./halal.jpg" alt="logo" width={55}/>
+                            <img src="./halal.jpg" alt="logo" width={45}/>
+                        </a>
+                        <a>
+                            <img src="./halal_supplier.jpg" alt="halal supplier's logo" width={90} />
                         </a>
                     </span>  
                 </div>    
